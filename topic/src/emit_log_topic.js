7136201242b7f9e1663863ce3d38b38cbfd9e262
Amir Marmul <amiruddinmarmul@gmail.com>
2022-11-03 15:25:28 +0800

    topic

---
+++ topic/src/emit_log_topic.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+  if (error0) {
+    throw error1;
+  }
+
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    var exchange = 'topic_logs';
+    var args = process.argv.slice(2);
+    var msg = args.slice(1).join(' ') || 'Hello World!';
+    var key = (args.length > 0) ? args[0] : 'anonymous.info';
+
+    channel.assertExchange(exchange, 'topic', {
+      durable: false,
+    });
+
+    channel.publish(exchange, key, Buffer.from(msg));
+
+    console.log(" [x] Sent %s: '%s'", key, msg);
+  });
+
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
